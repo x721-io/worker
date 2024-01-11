09e403bed4154cd1af2545edb2a564dce880150c
hieunm_u2u <hieunm@uniultra.xyz>
2024-01-11 09:45:19 +0700

    Job Tracking LaunchPad

--- codegen.ts
+++ codegen.ts
@@ -6,7 +6,10 @@ dotenvConfig();
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.SUBGRAPH_URL,
+  schema: [
+    process.env.SUBGRAPH_URL as string,
+    process.env.SUBGRAPH_URL_STAKING as string,  // Use type assertion to ensure non-undefined value
+  ],
   documents: 'src/**/*.graphql',
   generates: {
     "src/generated/graphql.ts": {
