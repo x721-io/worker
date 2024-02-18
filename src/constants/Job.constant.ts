d60c2572acf00f0a3f8a3bff7a108949e1b1f860
MacOs <dangkimbao1999@gmail.com>
2024-02-19 00:27:11 +0700

    [UPDATE] update floor price

--- src/constants/Job.constant.ts
+++ src/constants/Job.constant.ts
@@ -4,3 +4,4 @@ export const QUEUE_NAME_IPFS = 'marketplace-data-ipfs';
 export const QUEUE_NAME_PROJECT = 'project';
 export const QUEUE_NAME_TIMER = 'timer-schedule';
 export const QUEUE_NAME_USER = 'user';
+export const QUEUE_COLLECTION_UTILS = 'marketplace-collection-utils';
---
+++ src/modules/job-queue/collection-utils.processor.ts
@@ -0,0 +1,112 @@
+// import { Job } from 'kue';
+import { GraphQLClient } from 'graphql-request';
+import {
+  GetNfTsSelling1155QueryVariables,
+  GetNfTsSelling721QueryVariables,
+  getSdk,
+} from 'src/generated/graphql';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Processor, Process } from '@nestjs/bull';
+import { Job } from 'bull';
+import { QUEUE_COLLECTION_UTILS } from 'src/constants/Job.constant';
+
+interface FloorPriceProcess {
+  address: string;
+}
+
+@Processor(QUEUE_COLLECTION_UTILS)
+export class CollectionsUtilsProcessor {
+  private readonly endpoint = process.env.SUBGRAPH_URL;
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  private getGraphqlClient() {
+    return new GraphQLClient(this.endpoint);
+  }
+
+  @Process('update-floor-price')
+  private async updateFloorPrice(job: Job<FloorPriceProcess>) {
+    console.log(job.data);
+    await this.handleUpdateFloorPrice(job.data.address);
+  }
+
+  async handleUpdateFloorPrice(address: string) {
+    const collection = await this.prisma.collection.findUnique({
+      where: {
+        address,
+      },
+    });
+    if (!collection) {
+      console.error('Collection not found');
+      return;
+    }
+    // for (let i = 0; i < collections.length; i++) {
+    let skip = 0;
+    let hasMore = true;
+    let floorPrice = BigInt(0);
+    const first = 1000;
+    const client = this.getGraphqlClient();
+    const sdk = getSdk(client);
+    if (collection.type === 'ERC721') {
+      while (hasMore) {
+        const variables: GetNfTsSelling721QueryVariables = {
+          first,
+          skip,
+          collection: collection.address,
+        };
+        const response = await sdk.GetNFTsSelling721(variables);
+        if (response.marketEvent721S.length > 0) {
+          for (let i = 0; i < response.marketEvent721S.length; i++) {
+            if (floorPrice === BigInt(0)) {
+              floorPrice = response.marketEvent721S[i].price;
+            } else {
+              if (
+                BigInt(response.marketEvent721S[i].price) < BigInt(floorPrice)
+              ) {
+                floorPrice = response.marketEvent721S[i].price;
+              }
+            }
+          }
+          skip += first;
+        } else {
+          hasMore = false;
+        }
+      }
+    }
+    if (collection.type === 'ERC1155') {
+      while (hasMore) {
+        const variables: GetNfTsSelling1155QueryVariables = {
+          first,
+          skip,
+          collection: collection.address,
+        };
+        const response = await sdk.GetNFTsSelling1155(variables);
+        if (response.marketEvent1155S.length > 0) {
+          for (let i = 0; i < response.marketEvent1155S.length; i++) {
+            if (floorPrice === BigInt(0)) {
+              floorPrice = response.marketEvent1155S[i].price;
+            } else {
+              if (
+                BigInt(response.marketEvent1155S[i].price) < BigInt(floorPrice)
+              ) {
+                floorPrice = response.marketEvent1155S[i].price;
+              }
+            }
+          }
+          skip += first;
+        } else {
+          hasMore = false;
+        }
+      }
+    }
+    await this.prisma.collection.update({
+      where: {
+        address,
+      },
+      data: {
+        floorPrice: BigInt(floorPrice) / BigInt(10 ** 18),
+      },
+    });
+    return true;
+  }
+}
